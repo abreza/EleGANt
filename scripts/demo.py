afa6850ad2ecf59b14078e9bfaa2c7caa14ded54
reza <ab.reza@live.com>
2022-09-29 19:32:53 +0330

    change api

--- scripts/demo.py
+++ scripts/demo.py
@@ -18,22 +18,21 @@ def main(config, args):
 
     inference = Inference(config, args, args.load_path)
 
-    n_imgname = sorted(os.listdir(args.source_dir))
-    m_imgname = sorted(os.listdir(args.reference_dir))
-    
-    for i, (imga_name, imgb_name) in enumerate(zip(n_imgname, m_imgname)):
-        imgA = Image.open(os.path.join(args.source_dir, imga_name)).convert('RGB')
-        imgB = Image.open(os.path.join(args.reference_dir, imgb_name)).convert('RGB')
+    source = Image.open(args.source).convert('RGB')
+    lip_ref = Image.open(args.lip_ref).convert('RGB')
+    skin_ref = Image.open(args.skin_ref).convert('RGB')
+    eye_ref = Image.open(args.eye_ref).convert('RGB')
 
-        result = inference.transfer(imgA, imgB, postprocess=True) 
-        if result is None:
-            continue
-        imgA = np.array(imgA); imgB = np.array(imgB)
-        h, w, _ = imgA.shape
-        result = result.resize((h, w)); result = np.array(result)
-        vis_image = np.hstack((imgA, imgB, result))
-        save_path = os.path.join(args.save_folder, f"result_{i}.png")
-        Image.fromarray(vis_image.astype(np.uint8)).save(save_path)
+    result = inference.joint_transfer(source, lip_ref, skin_ref, eye_ref, postprocess=True) 
+    if result is None:
+        return
+    
+    imgA = np.array(imgA); imgB = np.array(imgB)
+    h, w, _ = imgA.shape
+    result = result.resize((h, w)); result = np.array(result)
+    vis_image = np.hstack((imgA, imgB, result))
+    save_path = os.path.join(args.save_folder, f"result_{i}.png")
+    Image.fromarray(vis_image.astype(np.uint8)).save(save_path)
 
 
 if __name__ == "__main__":
@@ -43,8 +42,10 @@ if __name__ == "__main__":
     parser.add_argument("--load_path", type=str, help="folder to load model", 
                         default='ckpts/sow_pyramid_a5_e3d2_remapped.pth')
 
-    parser.add_argument("--source-dir", type=str, default="assets/images/non-makeup")
-    parser.add_argument("--reference-dir", type=str, default="assets/images/makeup")
+    parser.add_argument("--source", type=str, default="assets/images/non-makeup/source_1.png")
+    parser.add_argument("--lip_ref", type=str, default="assets/images/makeup/reference_1.png")
+    parser.add_argument("--skin_ref", type=str, default="assets/images/makeup/reference_2.png")
+    parser.add_argument("--eye_ref", type=str, default="assets/images/makeup/reference_3.png")
     parser.add_argument("--gpu", default='0', type=str, help="GPU id to use.")
 
     args = parser.parse_args()
